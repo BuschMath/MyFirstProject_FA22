f7fac7d93e9e18e647829d82f7dc6b64fc4c996c
BuschMath <busch@iowacentral.edu>
2022-08-26 11:38:26 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,16 @@
+/*****************************
+	David Busch
+	C++ Fall 2022
+	Due : Aug. 31, 2022
+	Lab 1 Introduction to C++ Programming
+	This lab is a demo of my ability to work with an IDE and GitHub.
+******************************/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << " Blue";
+
+	return 0;
+}
